8fcd9fb1524bd88b76785e9a44da611f8ba3f567
MCeciliaLuna <lunama.cecilia@live.com>
2023-07-19 15:37:48 -0300

    páginas y root listo 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Login from "./routes/login";
+import Admin from "./routes/Admin";
+import Info from "./routes/Info";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/info",
+    element: <Info />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
